5ea8cab65c3b6a4829ea580f245096cc3812f242
sadia <sadia@techhivebd.com>
2020-08-11 18:12:04 +0600

    add unique room id

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid')
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-// res.status(200).send("hello world");
-//embedded js it will help us to get variable from frontend to backend
-res.render('room');
+  // res.status(200).send("hello world");
+  //embedded js it will help us to get variable from frontend to backend
+  // res.render('room');
+  res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
 
