1205ea5cf279a75f64c0cf38c0da343f77b80ad2
sadia <sadia@techhivebd.com>
2020-08-11 16:07:57 +0600

    add view engine ejs

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine','ejs');
+
 app.get('/', (req, res) => {
-res.status(200).send("hello world");
+// res.status(200).send("hello world");
+//embedded js it will help us to get variable from frontend to backend
+res.render('room');
 })
 
 
