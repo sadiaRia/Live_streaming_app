56198da4e1904bf69a549093a22430ee5a44e0f1
sadia <sadia@techhivebd.com>
2020-08-12 20:13:52 +0600

    add peerjs & get to know which specific user join to my room

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 const server = require('http').Server(app);
 const { v4: uuidv4 } = require('uuid')
 const io = require('socket.io')(server);
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 
 app.get('/', (req, res) => {
   // res.status(200).send("hello world");
@@ -18,9 +24,9 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-  socket.on('join-room', (roomId) => {
+  socket.on('join-room', (roomId, userId) => { //now we need listed that userId after emit
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected');
+    socket.to(roomId).broadcast.emit('user-connected', userId); //emit for every one that certain user just connected
     // console.log("joined room");
   })
 })
