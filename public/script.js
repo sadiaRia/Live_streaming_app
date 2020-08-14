efc8050e5efb180b4374edb1160e1d00b6d987cd
sadia <sadia@techhivebd.com>
2020-08-14 10:05:17 +0600

    add mute unmute functionality

--- public/script.js
+++ public/script.js
@@ -18,7 +18,7 @@ let myVideoStream;
 //access permission for own video
 navigator.mediaDevices.getUserMedia({
   video: true,
-  audio: false
+  audio: true
 }).then(stream => {
   myVideoStream = stream;
   addVideoStream(myVideo, stream);
@@ -91,4 +91,34 @@ const addVideoStream = (video, stream) => {
   })
   videoGrid.append(video);
 }
+
+//mute video
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+
 
